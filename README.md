9070f3d259cb1141c00d324ddea03f0582cb4d2e
João Vitor Nogueira <jvnogueira2010@gmail.com>
2024-03-22 05:00:47 -0300

    Desenvolvimento de bot para unfriend realizando integração com o arquivo.txt devido o alto volume de caracteres

---
+++ README.md
@@ -0,0 +1,47 @@
+# Facebook Unfriend Bot
+
+Projeto de bot personalizado para remover da lista de amigos automaticamente na plataforma do facebook, tendo diversas funcionalidades. Vale enfatizar que não há necessidade de conhecimento em programação para o download da extensão e ativação do bot
+
+## Funcionamento
+
+### Instalação e configuração da extensão
+1. Copiar a pasta do repositório remoto para o repositório local
+2. Acessar o seu navegador e clicar em 'Gerenciar extensões'. Habilitar o 'Modo do desenvolvedor' (no meu caso, utilizei o navegador Edge)
+3. Clicar em 'Carregar sem pacote' e em seguida abrir a pasta do repositório local
+4. Verificar se a extensão 'instagramUnfollow' está habilitada (conforme a imagem abaixo)
+
+### Configuração do bot no instagram
+1. Abrir o instagram e atualizar a página, clicando na tecla 'F5' localizada na parte superior do teclado
+2. Na aba do perfil do instagram, clicar em 'Seguindo' ou 'Seguidores' (conforme a imagem abaixo)
+3. Após a página estar totalmente carregada, clique na tecla 'F2'
+4. Ao abrir a mensagem de pop-up (conforme a imagem abaixo), insira a quantidade de seguidores que o bot vai 'Deixar de seguir' ou 'Remover' e clique em 'Ok' ou aperte 'Enter'
+5. Insira quantos segundos para durar cada ciclo de intervalo de tempo entre cada unfollow
+6. Insira quais usernames para o bot evitar o unfollow e clique em 'Ok' ou aperte 'Enter'
+7. O bot vai parar o funcionamento automaticamente nesses casos: ao atingir o limite de usernames que deixou de seguir ou removeu, quando sair da tela de funcionamento ou quando o bot percorrer todos os perfis. O bot vai voltar a funcionar quando clicar em 'F2' novamente
+8. Ao atingir o limite de usernames, então vai aparecer a mensagem em pop-up perguntando se deseja realizar o download do arquivo (unfriendList.txt) que contêm o nome e username que o bot deixou de seguir ou removeu. É uma ótima maneira para revisar os perfis. Para quem quer algo ainda mais organizado, o bloco de notas em txt já vai estar com os dados espaçados, permitindo que ao copiar e colar em uma planilha fiquem organizados em colunas "Nome" e "URL"
+9. Não há necessidade de utilizar o editor de código para configurar o bot, mas quem estiver interessado em contribuir realizando implementações, fique a vontade. Não se esqueça de dar uma estrela ao projeto
+
+
+
+## Observações
+
+- O grupo Meta (facebook, instagram, whatsapp, messenger, etc.) tem restrições com o uso de bots, então use por conta e risco. 
+- Uma recomendação, seria evitar deixar de seguir muitas pessoas na mesma hora. 
+  - No máximo um unfollow a cada 60 segundos (60 unfollow por hora)
+  - No máximo 200 unfollow por dia
+  - Para saber mais sobre isso:
+    - [clique aqui](https://portalpopclub.com.br/redes-sociais/limites-instagram-por-dia/)
+    - [e aqui](https://www.pathsocial.com/pt/resources/spring-clean-how-many-people-can-you-unfollow-on-instagram/)
+- O bot foi criado com o objetivo de diminuir as ações manuais, melhorar a organização e aumentar o tempo livre. Ou seja, sem objetivos maliciosos
+- Deixe a página do instagram ativa enquanto o bot estiver em funcionamento, pois caso contrário, o bot pode não funcionar corretamente
+- Não tem problema desligar o monitor enquanto o bot estiver em funcionamento, mas configure a economia de energia do seu PC para que a CPU não desligue sozinha por inatividade
+- O bot foi testado em um sistema operacional Windows 10 Pro
+- O idioma do instagram que o bot foi testado é o Português (Brasil). O bot pode não funcionar corretamente em outro idioma
+- Uma dica para ajudar na organização dos usernames é utilizar o bot que pega todos os usernames que o perfil do instagram está seguindo para salvar em uma planilha ou bloco de notas, segue o link do projeto: [www.github.com/jv-nogueira/getInstagramUsernameAll.com](https://github.com/jv-nogueira/getInstagramUsernameAll.git)
+- O bot foi desenvolvido com uma variação entre cada ciclo dos intervalos de tempo para assim se aproximar com o comportamento humano
+
+## Contato
+
+- E-mail: [jvnogueira2010@gmail.com](mailto:jvnogueira2010@gmail.com)
+- LinkedIn: [www.linkedin.com/in/nogueira-jv](https://www.linkedin.com/in/nogueira-jv)
+- GitHub: [www.github.com/jv-nogueira](https://github.com/jv-nogueira)
---
+++ content.js
@@ -0,0 +1,135 @@
+
+document.addEventListener("keydown", function(event) {
+    
+    fetch(chrome.runtime.getURL('urls.txt'))
+    .then(response => response.text())
+    .then(text => {
+ 
+    var standardMessage = "\n\nSupport: jvnogueira2010@gmail.com";
+    if(document.location.href.includes('friends')){
+        if (event.keyCode === 113) {
+            var question = prompt("Digite no campo abaixo a quantidade de pessoas que o bot vai remover dos amigos:"+
+            standardMessage)
+        }
+    };
+    if(question > 0){
+        var questionTemp = prompt("Digite no campo abaixo quantos segundos quer que dure cada ciclo de intervalo de tempo para o unfollow. Lembrando que quanto maior o intervalo de tempo, mais seguidores o bot vai remover da lista de amigos sem ser impedido pelo instagram."+
+        standardMessage)
+        if(!isNaN(questionTemp) && questionTemp > 10){
+            var answerTemp = questionTemp/2 // Pega a resposta do prompt e divide por 2 para dividir igualmente os segundos entre os intervalos
+            var index=0
+            var contadorUnfriend=0
+            var arrayArmazenarNameAndURL = [];
+            percorrer()
+            function percorrer(){
+
+                var lastIndex = document.querySelectorAll('[data-pagelet="ProfileAppSection_0"]')[0].children[0].children[0].children[0].children[0].children[2].children.length -2 // Referencia o last index da lista de perfis
+
+                var lastElementIndex = document.querySelectorAll('[data-pagelet="ProfileAppSection_0"]')[0].children[0].children[0].children[0].children[0].children[2].children[lastIndex] // Visualiza o last element do index da lista de perfis
+
+                var profileReference = document.querySelectorAll('[data-pagelet="ProfileAppSection_0"]')[0].children[0].children[0].children[0].children[0].children[2].children[index] // Referencia o perfil
+
+                var profileURL = profileReference.children[1].children[0].children[0].href // Retorna o URL do perfil de acordo com o index
+                
+                var buttonUnfriend = profileReference.children[2].children[0].children[0].children[0] // Clica no três pontinhos
+                
+                // var removeConfirm = document.querySelectorAll("[role='dialog']")[0].children[0].children[0].children[0].children[2].children[0].children[0].children[0].children[0].children[0].children[0] // Clica no botão confirmar do pop-up
+
+                var getName = profileReference.children[1].children[0].children[0].children[0].innerHTML
+
+                var getNameAndURL = {getName, profileURL}
+
+                var viewProfile = profileReference.scrollIntoView() // Rola a página para que o elemento fique vísivel 
+                
+                var limiteContador = question;
+
+                    var usernamesPermitidos = text
+                    if(!usernamesPermitidos.includes(profileURL)){
+                        viewProfile 
+                        if(contadorUnfriend < limiteContador){ // Limite de perfis que o bot vai remover da lista de amigos
+                            arrayArmazenarNameAndURL.push(getNameAndURL)
+                            setTimeout(() => {
+                                buttonUnfriend.click()
+                                setTimeout(() => {
+                                    var removeFriend = document.querySelectorAll("[role='menuitem']")[3] // Clica na opção 'Remover amizade' da lista
+                                    removeFriend.click()
+                                    contadorUnfriend++
+                                    index++
+                                    percorrer()
+                                },getRandomSeconds(answerTemp))
+                            },getRandomSeconds(answerTemp))
+                        }else{
+                            setTimeout(() => {
+                            var removeWhiteSpace = limiteContador.replace(/\s/g, '')
+                            var singular = "" 
+                            var plural = ""
+                            if(limiteContador > 1){
+                                plural = "s"
+                            }else{
+                                singular = ""
+                            };
+                            var questionFinal = confirm("Chegou ao limite de "+removeWhiteSpace+" pessoa"+singular+plural+" que o bot removeu da lista de amigos!\n\n"+
+                            "Deseja a lista com as pessoas que o bot removeu da lista de amigos? Se sim, clique em 'Ok' ou aperte 'Enter' para o download do arquivo 'unfriendList.txt'"+
+                            standardMessage)
+                            if(questionFinal == true){
+                                // Criar o conteúdo do TXT
+                                var txtContent = "\uFEFFNome\tUsername\n";
+                                arrayArmazenarNameAndUsername.forEach(function (item) {
+                                txtContent += item.getName + "\t" + item.profileURL + "\n";
+                                });
+
+                                // Criar um Blob com o conteúdo do TXT
+                                var blob = new Blob([txtContent], { type: "text/txt" });
+
+                                // Criar um link para o Blob
+                                var link = document.createElement("a");
+                                link.href = window.URL.createObjectURL(blob);
+
+                                // Definir o nome do arquivo
+                                link.download = "unfriendList.txt";
+
+                                // Adicionar o link à página e clicar automaticamente para iniciar o download
+                                document.body.appendChild(link);
+                                link.click();
+
+                                // Remover o link da página
+                                document.body.removeChild(link);
+                            }
+                            },getRandomSeconds(answerTemp))
+                        };
+                        
+                    }else if(index < lastIndex){
+                        index++
+                        percorrer()
+                    }else{
+                        setTimeout(() => {
+                            lastElementIndex.scrollIntoView()
+                            setTimeout(() => {
+                                percorrer()
+                            },getRandomSeconds(1))
+                        },getRandomSeconds(1))
+                    }
+            }
+        }else if(questionTemp <= 10){
+            alert('Digite um número acima de 10!'+
+            standardMessage)
+        }else if(typeof questionTemp == 'string'){
+            alert('Digite apenas números!'+
+            standardMessage)
+        }; 
+    }else if(typeof question == 'string' && question != 0){
+            alert('Digite apenas números!'+
+            standardMessage)
+        }else if(question == 0){
+            alert('Digite um número acima de 0!'+
+            standardMessage)
+        }; 
+    
+
+function getRandomSeconds(sum){
+    let sec222 = Math.random() * 4 + sum; // Multiplica um random number e soma 
+    sec222 =  parseFloat(sec222.toFixed(3))*1000; // Diminui para 3 casas decimais, converte de string para number e multiplica por 1000 que corresponde a 1 segundo
+    return sec222; // retorna o resultado aleatorio em segundos 
+};
+})
+});
